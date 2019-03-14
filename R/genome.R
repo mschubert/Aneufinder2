0ab9070f010d8842819a8f2f4b803ec3dccccdf7
mschubert <mschu.dev@gmail.com>
2019-03-14 14:14:36 +0100

    handle BamFile, filter via .seqinfo

--- R/genome.R
+++ R/genome.R
@@ -1,15 +1,28 @@
-#TODO: remove nonstandard chromosomes by default
-
 genome <- function(x, ...) {
     UseMethod("genome")
 }
 
 genome.character <- function(x, ...) {
+    if (length(x) > 1)
+        stop("Can only get genome information for one object at a time")
+    if (file.exists(x) && grepl("\\.bam$", x))
+        genome(Rsamtools::BamFile(x), ...)
+
     #TODO: get seqinfo object from assembly identifier
 }
 
 genome.GRanges <- function(x, ...) {
-    GenomeInfoDB::seqinfo(x, ...)
+    seqinfo <- GenomeInfoDB::seqinfo(x)
+    genome(seqinfo, ...)
+}
+
+genome.BamFile <- function(x, ...) {
+    seqinfo <- GenomeInfoDB::seqinfo(x)
+    seqinfo(seqinfo, ...)
+}
+
+genome.seqinfo <- function(x, ...) {
+    #TODO: remove nonstandard chromosomes by default
 }
 
 genome.default <- function(x, ...) {
