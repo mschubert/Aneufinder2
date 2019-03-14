56b45eaba71f9e56118767fd7cf90bbc84520599
mschubert <mschu.dev@gmail.com>
2019-03-14 13:58:47 +0100

    handle width ref via arg, not S3

--- R/binGenome.R
+++ R/binGenome.R
@@ -1,39 +1,37 @@
 #' Partition the genome into bins
 #'
 #' @param variableWidthReference
-binGenome <- function(x, bin.size=1e6, reads.per.bin=NULL, stepsize=NULL) {
+binGenome <- function(seqinfo, bin.size=1e6, reads.per.bin=NULL, stepsize=NULL,
+                      variable.ref=NULL) {
     UseMethod("binGenome")
 }
 
-# fixed bins
-binGenome.seqinfo <- function(x, bin.size=1e6, stepsize=NULL) {
-    chrom.lengths = GenomeInfoDB::seqlengths(seqinfo)
-    if (stepsize == binsize) { # non-overlapping bins
-        chrom.lengths.floor <- floor(chrom.lengths / binsize) * binsize
-    } else if (stepsize < binsize) { # overlapping bins
-        chrom.lengths.floor <- floor(chrom.lengths / stepsize) * stepsize
-        chrom.lengths.floor <- chrom.lengths.floor - (((binsize/stepsize)-1)*stepsize)
-        chrom.lengths.floor <- pmax(chrom.lengths.floor, 0)
+binGenome.seqinfo <- function(seqinfo=genome(varable.ref), bin.size=1e6,
+                              stepsize=NULL, variable.ref=NULL) {
+
+    if (is.null(variable.ref)) {
+        chr.len = GenomeInfoDB::seqlengths(seqinfo)
+        if (stepsize == binsize) { # non-overlapping bins
+            chr.len.floor <- floor(chr.len / binsize) * binsize
+        } else if (stepsize < binsize) { # overlapping bins
+            chr.len.floor <- floor(chr.len / stepsize) * stepsize
+            chr.len.floor <- chr.len.floor - (((binsize/stepsize)-1)*stepsize)
+            chr.len.floor <- pmax(chr.len.floor, 0)
+        }
+
+        bins <- unlist(GenomicRanges::tileGenome(seqlengths=chr.len.floor,
+                                                 tilewidth=stepsize), use.names=FALSE)
+
+        if (stepsize < binsize)
+            end(bins) <- (start(bins) + binsize - 1)
+    } else {
+        stop("not implemented")
+        #TODO: handle variable width ref
     }
-
-    bins <- unlist(GenomicRanges::tileGenome(seqlengths=chrom.lengths.floor,
-                                             tilewidth=stepsize), use.names=FALSE)
-
-    if (stepsize < binsize)
-        end(bins) <- (start(bins) + binsize - 1)
-
 }
 
-# variable bins
-binGenome.GRanges <- function(x, seqinfo=genome(x), bin.size=1e6, stepsize=NULL) {
-    strand(reads) <- '*'
-    reads <- sort(reads)
-
-    mediancount <- as.integer(median(binned.list[[ibss]]$counts[binned.list[[ibss]]$counts>0]))
-}
-
-binGenome.character <- function(x, ...) {
-    binGenome(readGRanges(x), ...)
+binGenome.character <- function(seqinfo, ...) {
+    binGenome(genome(x), ...)
 }
 
 binGenome.default <- function(x, ...) {
