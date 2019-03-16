c00d6fdbeb13dcd617b51bde0f36a6c588b43d3e
mschubert <mschu.dev@gmail.com>
2019-03-16 15:50:05 +0100

    add util code

--- R/util.R
+++ R/util.R
@@ -36,3 +36,26 @@ match_call_defaults = function (call = match.call(sys.function(sys.parent()),
     call[missing_names] = missing_values
     call
 }
+
+messageU <- function(..., underline='=', overline='=') {
+    x <- paste0(..., collapse='')
+    if (!is.null(overline)) {
+        message(rep(overline, nchar(x)))
+    }
+    message(x)
+    if (!is.null(underline)) {
+        message(rep(underline, nchar(x)))
+    }
+}
+
+startTimedMessage <- function(...) {
+    x <- paste0(..., collapse='')
+    message(x, appendLF=FALSE)
+    ptm <- proc.time()
+    return(ptm)
+}
+
+stopTimedMessage <- function(ptm) {
+    time <- proc.time() - ptm
+    message(" ", round(time[3],2), "s")
+}
