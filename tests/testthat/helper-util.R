ed38328d87eeef008e0800f6cebda22d38a94ba5
mschubert <mschu.dev@gmail.com>
2019-03-16 16:37:16 +0100

    split file paths to util

---
+++ tests/testthat/helper-util.R
@@ -0,0 +1,3 @@
+fdir = system.file("extdata", package="AneuFinderData")
+bam = file.path(fdir, "BB150803_IV_074.bam")
+bed = file.path(fdir, "KK150311_VI_07.bam.bed.gz")
--- tests/testthat/test-readGRanges.R
+++ tests/testthat/test-readGRanges.R
@@ -1,9 +1,5 @@
 context("reading files")
 
-fdir = system.file("extdata", package="AneuFinderData")
-bam = file.path(fdir, "BB150803_IV_074.bam")
-bed = file.path(fdir, "KK150311_VI_07.bam.bed.gz")
-
 test_that("reading bam file into GRanges", {
     ranges = readGRanges(bam)
     expect_equal(c(class(ranges)), "GRanges")
