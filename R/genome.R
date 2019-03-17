30b01fba96ce6ea22e6b498fdf79664c5057d085
mschubert <mschu.dev@gmail.com>
2019-03-17 17:06:47 +0100

    basic seqinfo working

--- R/genome.R
+++ R/genome.R
@@ -9,7 +9,7 @@ genome.character <- function(x, ...) {
     if (file.exists(x) && grepl("\\.bam$", x))
         genome(Rsamtools::BamFile(x), ...)
     else
-        genome(GenomeInfoDb::fetchExtendedChromInfoFromUCSC(x), ...)
+        genome(GenomeInfoDb::Seqinfo(genome=x), ...)
 }
 
 genome.GRanges <- function(x, ...) {
@@ -19,15 +19,15 @@ genome.GRanges <- function(x, ...) {
 
 genome.BamFile <- function(x, ...) {
     seqinfo <- GenomeInfoDb::seqinfo(x)
-    seqinfo(seqinfo, ...)
+    genome(seqinfo, ...)
 }
 
-genome.seqinfo <- function(x, chrs=NULL) {
+genome.Seqinfo <- function(x, chrs=NULL) {
     if (is.null(chrs)) {
         chrs = GenomeInfoDb::standardChromosomes(x)
-        chrs = grep("^(chr)?[YM]$", chrs, invert=TRUE, value=TRUE)
+        chrs = grep("^(chr)?(Y|MT)$", chrs, invert=TRUE, value=TRUE)
     }
-    GenomeInfoDb::keepSeqlevels(x, chrs, pruning.mode="coarse")
+    GenomeInfoDb::keepSeqlevels(x, chrs)
 }
 
 genome.default <- function(x, ...) {
--- tests/testthat/test-genome.R
+++ tests/testthat/test-genome.R
@@ -1,4 +1,18 @@
-context("parition genome")
+context("genome seqinfo")
 
-test_that("", {
+test_that("genome information from bam file", {
+    seqinfo = genome(bam)
+
+    expect_setequal(names(seqinfo), c(1:19,'X'))
+    expect_equal(GenomeInfoDb::seqlengths(seqinfo)[[1]], 195471971)
+    expect_true(is.na(unique(GenomeInfoDb::genome(seqinfo)))) # not named
+})
+
+test_that("mm10", {
+    seqinfo = genome("mm10")
+
+    expect_equal(names(seqinfo), paste0("chr", c(1:19,'X')))
+    expect_equal(GenomeInfoDb::seqlengths(seqinfo)[[1]], 195471971)
+    expect_false(unique(GenomeInfoDb::isCircular(seqinfo)))
+    expect_equal(unique(GenomeInfoDb::genome(seqinfo)), "mm10")
 })
