74018a5be858094f71d9e76440f2aaab3426298b
mschubert <mschu.dev@gmail.com>
2019-03-18 12:24:32 +0100

    test fixed width bins

--- R/partitionGenome.R
+++ R/partitionGenome.R
@@ -2,33 +2,46 @@
 #'
 #' @param variableWidthReference
 partitionGenome <- function(seqinfo, bin.size=1e6, reads.per.bin=NULL,
-                            stepsize=NULL, variable.width.ref=NULL) {
+                            stepsize=NULL) {
     UseMethod("partitionGenome")
 }
 
-partitionGenome.seqinfo <- function(seqinfo=genome(variable.width.ref), bin.size=1e6,
-                              stepsize=NULL, variable.width.ref=NULL) {
+partitionGenome.Seqinfo <- function(seqinfo, bin.size=1e6, stepsize=NULL) {
+    if (is.null(stepsize))
+        stepsize <- bin.size
+    if (! (bin.size >= stepsize && bin.size %% stepsize == 0))
+        stop("bin.size must be a multiple of stepsize")
 
-    if (is.null(variable.width.ref)) { # fixed-size bins
-        chr.len = GenomeInfoDb::seqlengths(seqinfo)
-        if (stepsize == binsize) { # non-overlapping bins
-            chr.len.floor <- floor(chr.len / binsize) * binsize
-        } else if (stepsize < binsize) { # overlapping bins
-            chr.len.floor <- floor(chr.len / stepsize) * stepsize
-            chr.len.floor <- chr.len.floor - (((binsize/stepsize)-1)*stepsize)
-            chr.len.floor <- pmax(chr.len.floor, 0)
-        }
-
-        bins <- GenomicRanges::tileGenome(seqlengths=chr.len.floor, tilewidth=stepsize)
-        bins <- unlist(bins, use.names=FALSE)
-
-        if (stepsize < binsize) #FIXME: shouldn't we do this regardless?
-            end(bins) <- (start(bins) + binsize - 1)
-
-    } else { # variable bins
-        stop("not implemented")
-        #TODO: handle variable width ref
+    # fixed-size bins
+    chr.len <- GenomeInfoDb::seqlengths(seqinfo)
+    chr.len.floor <- floor(chr.len / stepsize) * stepsize
+    # overlapping bins
+    if (stepsize < bin.size) {
+        chr.len.floor <- chr.len.floor - (bin.size/stepsize - 1) * stepsize
+        chr.len.floor <- pmax(chr.len.floor, 0)
     }
+
+    bins <- GenomicRanges::tileGenome(seqlengths=chr.len.floor, tilewidth=stepsize)
+    bins <- unlist(bins, use.names=FALSE)
+    if (stepsize < bin.size)
+        end(bins) <- (start(bins) + bin.size - 1)
+
+    GenomeInfoDb::seqinfo(bins) <- seqinfo
+    bins
+}
+
+partitionGenome.GRanges <- function(seqinfo, bin.size=1e6, stepsize=NULL) {
+    stop("not implemented")
+    # seqinfo = GRanges with counts from variable width ref?
+    #  would need fixed bins + read counts from readGRanges(variable width ref)
+
+    mediancount <- as.integer(median(bins$counts[bins$counts>0]))
+    mc.perstep <- unique(as.integer((1:numsteps) * mediancount / numsteps))
+
+    chrs <- split(bins, GenomicRanges::seqnames(bins))
+    margins <- lapply(chrs, function(x) x[seq(mc.perstep, length(x), by=mediancount)])
+    margins <- unlist(margins)
+#    bins <- 
 }
 
 partitionGenome.character <- function(seqinfo, ...) {
--- tests/testthat/test-partitionGenome.R
+++ tests/testthat/test-partitionGenome.R
@@ -1,10 +1,16 @@
 context("parition genome")
 
 test_that("fixed width bins", {
+    genome = genome("mm10")
+    bins = partitionGenome(genome, bin.size=1e6)
+
+    expect_equal(length(bins), 2625)
+    expect_equal(GenomeInfoDb::seqinfo(bins), genome)
+    expect_setequal(as.character(GenomeInfoDb::seqnames(bins)), names(genome))
 })
 
-test_that("variable width bins", {
-})
+#test_that("variable width bins", {
+#})
 
-test_that("error on genome mismatch", {
-})
+#test_that("error on genome mismatch", {
+#})
