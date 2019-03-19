a0b2bb6b3d51fc7f5bad04f64c7f46c49ff3bed8
mschubert <mschu.dev@gmail.com>
2019-03-19 13:50:45 +0100

    simplify

--- R/binReads.R
+++ R/binReads.R
@@ -4,22 +4,16 @@ binReads <- function(reads, bins) {
 }
 
 binReads.character <- function(reads, bins) {
-    bin_one <- function(r, b) {
-        gr = readGRanges(r)
-        binReads(gr, b)
-    }
-
     if (length(reads) == 1 && dir.exists(reads)) {
         reads = list.files(reads, "\\.(bam|bed(\\.gz)?)$", full.names=TRUE)
         message("Reading ", length(reads), " sequence files ...")
     }
-
     names(reads) = tools::file_path_sans_ext(basename(reads))
 
     if (length(reads) > 1)
         binReads(as.list(reads), bins)
     else
-        bin_one(reads, bins)
+        binReads(readGRanges(reads), bins)
 }
 
 binReads.GRanges <- function(reads, bins) {
