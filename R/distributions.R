4d8c6163b4dae63c89da7d04f703a280fe61bccc
mschubert <mschu.dev@gmail.com>
2019-03-19 16:57:29 +0100

    merge distribution files

--- R/distributions.R
+++ R/distributions.R
@@ -1,3 +1,49 @@
+# Functions for a Negative Binomial to transform (mean,variance)<->(size,prob)
+
+dnbinom.size <- function(mean, variance) {
+	mean^2 / (variance - mean)
+}
+
+dnbinom.prob <- function(mean, variance) {
+	mean/variance
+}
+
+dnbinom.mean <- function(size, prob) {
+	size/prob - size
+}
+
+dnbinom.variance <- function(size, prob) {
+	(size - prob*size) / prob^2
+}
+
+dgeom.prob <- function(mean) {
+  1/(1+mean)
+}
+
+dgeom.mean <- function(prob) {
+	(1-prob)/prob
+}
+
+dgeom.variance <- function(prob) {
+	(1-prob)/prob^2
+}
+
+dbinom.size <- function(mean, variance) {
+	mean^2/(mean-variance)
+}
+
+dbinom.prob <- function(mean, variance) {
+	(mean-variance)/mean
+}
+
+dbinom.mean <- function(size, prob) {
+	size*prob
+}
+
+dbinom.variance <- function(size, prob) {
+	size*prob * (1-prob)
+}
+
 assign.distributions <- function(counts, states) {
     distributions <- list()
     # --> Determine distribution type based on mu and variance.
@@ -44,7 +90,6 @@ assign.distributions <- function(counts, states) {
                     }
                 }
             }
-
             distributions[[i1]] <- data.frame(type=distr, size=size, prob=prob,
                                               mu=mu, variance=variance)
         }
