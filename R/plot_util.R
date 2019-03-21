82fb5e70d7e4367cba5e17dc2ae7d50e6c679619
mschubert <mschu.dev@gmail.com>
2019-03-21 13:17:21 +0100

    whitespace

--- R/plot_util.R
+++ R/plot_util.R
@@ -74,7 +74,12 @@ plot.GRangesList <- function(x, type='profile', ...) {
 #' statecolors <- stateColors()
 #' pie(rep(1,length(statecolors)), labels=names(statecolors), col=statecolors)
 stateColors <- function(states=c('zero-inflation', paste0(0:10, '-somy'), 'total')) {
-    state.colors <- c("zero-inflation"="gray90", "0-somy"="gray90","1-somy"="darkorchid3","2-somy"="springgreen2","3-somy"="red3","4-somy"="gold2","5-somy"="navy","6-somy"="lemonchiffon","7-somy"="dodgerblue","8-somy"="chartreuse4","9-somy"="lightcoral","10-somy"="aquamarine2","total"="black")
+    state.colors <- c("zero-inflation"="gray90", "0-somy"="gray90",
+                      "1-somy"="darkorchid3", "2-somy"="springgreen2",
+                      "3-somy"="red3", "4-somy"="gold2", "5-somy"="navy",
+                      "6-somy"="lemonchiffon", "7-somy"="dodgerblue",
+                      "8-somy"="chartreuse4", "9-somy"="lightcoral",
+                      "10-somy"="aquamarine2", "total"="black")
     states.with.color <- intersect(states, names(state.colors))
     cols <- rep('black', length(states))
     names(cols) <- states
