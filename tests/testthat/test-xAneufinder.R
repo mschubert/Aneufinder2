fb7e690fb0e02a2f91a9d1f220a2dea4c3c1b9b3
mschubert <mschu.dev@gmail.com>
2019-04-30 14:59:42 +0200

    integrated test upd

--- tests/testthat/test-xAneufinder.R
+++ tests/testthat/test-xAneufinder.R
@@ -3,34 +3,15 @@ context("Aneufinder main function")
 test_that("integrated run", {
     skip("interactive test")
 
-#library(AneuFinder2)
 library(GenomicRanges)
 library(GenomeInfoDb)
 library(ggplot2)
 source("helper-util.R")
-source("../../R/Aneufinder.R")
-source("../../R/rwConfig.R")
-source("../../R/checkClass.R")
-source("../../R/readGRanges.R")
-source("../../R/genome.R")
-source("../../R/partitionGenome.R")
-source("../../R/addGCcontent.R")
-source("../../R/correctGC.R")
-source("../../R/binReads.R")
-source("../../R/findCNVs.R")
-source("../../R/findCNVs_edivisive.R")
-source("../../R/findCNVs_HMM.R")
-source("../../R/util.R")
-source("../../R/distributions.R")
-source("../../R/util.R")
-source("../../R/genomeBins.R")
-source("../../R/adjustBins.R")
-source("../../R/collapseBins.R") #TODO: rewrite
+for (fname in list.files("../../R", "\\.R", full.names=TRUE))
+    source(fname)
+#source("../../R/collapseBins.R") #TODO: rewrite
 #source("../../R/backup/clusterHMMs.R")
 #source("../../R/backup/qualityMeasures.R")
-source("../../R/plotKaryogram.R") #TODO: remove this + replace by commented code in plotKaryograms
-source("../../R/plotKaryograms.R")
-source("../../R/plotProfile.R")
 
     dir = tempdir()
     models = Aneufinder(bam, assembly="GRCm38", outputfolder=dir,
